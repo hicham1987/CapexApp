a60a7d6fd8e1ce4f531b42bed8d5cb7c0aac6146
hicham <hicham.alaoui@interticket.net>
2018-04-29 11:38:24 -0400

    Situation Ao et Rfi

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core'
 
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { EmployeeService } from '../shared/employee.service';
 import { AuthService } from './../../auth.service';
 import { ToastrService } from 'ngx-toastr';
-import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-employee',
@@ -46,26 +46,40 @@ export class EmployeeComponent implements OnInit {
       employeeForm.reset();
     this.employeeService.selectedEmployee = {
       $key: null,
-      numDa : null,
-      designation : '',
-      numDossier : null,
+      numDa: null,
+      designation: '',
+      numDossier: null,
       chefP: '',
-      estimation:null,
-      numRfi:null,
-      situationRfi:'',
-      numAo :null,
-      situationAo :'',
-      montantDeLaCad :0,
+      estimation: null,
+      numRfi: null,
+      situationRfi: '',
+      numAo: null,
+      situationAo: '',
+      montantDeLaCad: 0,
     }
   }
 
-  foods = [
+  Rfis = [
+    { value: 'annulé', viewValue: 'annulé' },
+    { value: 'en-cours-de-preparation', viewValue: 'en-cours-de-preparation' },
+    { value: 'publié', viewValue: 'publié' },
+    { value: 'à ouvrir', viewValue: 'à ouvrir' },
+    { value: 'attente evaluation RFI', viewValue: 'attente evaluation RFI' },
+    { value: 'attente CGAO', viewValue: 'attente CGAO' },
+    { value: 'attente GEL', viewValue: 'attente GEL' },
+    { value: 'attente lancement AO', viewValue: 'attente lancement AO' }
+  ];
+
+  Aos = [
     {value: 'annulé', viewValue: 'annulé'},
-    {value: 'en-cours-de-preparation', viewValue: 'en-cours-de-preparation'},
     {value: 'publié', viewValue: 'publié'},
     {value: 'à ouvrir', viewValue: 'à ouvrir'},
     {value: 'attente avis technique', viewValue: 'attente avis technique'},
-    {value: 'étude commercial', viewValue: 'étude commercial'}
+    {value: 'étude commercial', viewValue: 'étude commercial'},
+    {value: 'attente CAD', viewValue: 'attente CAD'},
+    {value: 'attente AE', viewValue: 'attente AE'},
+    {value: 'attente validation contrat-Attribué ', viewValue: 'attente validation contrat-Attribué'},
+    {value: 'Soldé', viewValue: 'Soldé'},
   ];
 
 
