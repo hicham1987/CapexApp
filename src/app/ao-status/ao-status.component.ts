7ec1ffbfef8a1e626adb447c98c906596db1b360
hicham <hicham.alaoui@interticket.net>
2018-04-29 12:42:50 -0400

    cleaning

--- src/app/ao-status/ao-status.component.ts
+++ src/app/ao-status/ao-status.component.ts
@@ -5,6 +5,9 @@ import { ToastrService } from 'ngx-toastr';
 import { EmployeesComponent } from '../employees/employees.component'
 import { AuthService } from './../auth.service';
 import {MatCardModule} from '@angular/material/card';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Employee } from '../employees/shared/employee.model';
+
 
 @Component({
   selector: 'app-ao-status',
@@ -12,16 +15,22 @@ import {MatCardModule} from '@angular/material/card';
   styleUrls: ['./ao-status.component.css']
 })
 export class AoStatusComponent implements OnInit {
-
+  employeeList: Employee[];
+  selecteduser;
+  public users;
 
   constructor(
     private dataService: EmployeeService,
+    private employeeService: EmployeeService,
     private tostr: ToastrService,
-    private auth: AuthService
+    private db: AngularFireDatabase,
+    private auth: AuthService,
   ) { }
 
   ngOnInit() {
     this.resetForm();
+    this.loadUsers();
+    this.loadEmployeeData();
   }
 
   onSubmit(dataForm: NgForm) {
@@ -57,4 +66,41 @@ export class AoStatusComponent implements OnInit {
       montantDeLaCad :0,
     };
   }
+  loadEmployeeData(uid = null) {
+    const x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(element => {
+        const y: any = element.payload.toJSON();
+        const uuid = uid ? uid : this.auth.loggedInUser.uid;
+        if (y.creatorUid === uuid) {
+          y['$key'] = element.key;
+          this.employeeList.push(y as Employee);
+        }
+      });
+    });
+  }
+  loadUsers() {
+    const u = this.db.list('users/');
+    u.snapshotChanges().subscribe(item => {
+      this.users = [];
+      item.forEach(element => {
+        const y: any = element.payload.toJSON();
+        y['$key'] = element.key;
+        this.users.push(y);
+      });
+    });
+  }
+  onUserSelected(data) {
+    const uid = data.value['$key'];
+    this.loadEmployeeData(uid);
+  }
+
+  isAdmin(): boolean {
+    if (!this.auth.loggedInUser) {
+      return false;
+    }
+    return ( 'isAdmin' in this.auth.loggedInUser && this.auth.loggedInUser['isAdmin']);
+  }
+
 }
